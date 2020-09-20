8c61135d448d5101742b90658d40ba94e25c6378
Fabian <fabian13pl@gmail.com>
2020-09-20 13:53:44 +0200

    2

---
+++ car_trade_machine/src/com/company/CarGenerator.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class CarGenerator {
+
+    public static void carGenerator(String[] args) {
+        private String brand[3];
+        brand[0]="Audi";
+        brand[1]="Honda";
+        brand[2]="Fiat";
+
+        private String color[3];
+        color[0]="black";
+        color[1]="white";
+        color[2]="red";
+
+        private String segment[3];
+        segment[0]="premium";
+        segment[1]="standart";
+        segment[2]="budget";
+
+        private int value;
+        private int car_mileage;
+
+        Car car = new Car();
+    }
+}
